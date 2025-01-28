bbaf77d4d18fb52a594a5c9b01f8ada34d5d2595
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-28 19:45:27 +0900

    remove errors

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/error-overlay/error-overlay.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/error-overlay/error-overlay.tsx
@@ -39,7 +39,7 @@ export function ErrorOverlay({
   }
 
   // No Runtime Errors.
-  if (!state.errors.length) {
+  if (!readyErrors.length) {
     return null
   }
 
@@ -52,7 +52,6 @@ export function ErrorOverlay({
       debugInfo={state.debugInfo}
       hasStaticIndicator={state.staticIndicator}
       isTurbopack={isTurbopack}
-      errors={state.errors}
       readyErrors={readyErrors}
       versionInfo={state.versionInfo}
       onClose={() => {
